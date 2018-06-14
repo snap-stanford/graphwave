c76708eeb7c26837baff3ac9c3da48f762378af8
donnate <claire.donnat@gmail.com>
2018-06-14 00:22:37 -0700

    cleanup distances

--- graphwave/distances/comparison_char_functions.py
+++ graphwave/distances/comparison_char_functions.py
@@ -3,25 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def compare_simple_diffusion(A):
-    N = A.shape[0]
-    heat_signature = pd.TimeSeries(np.zeros(N), index=range(N))
-    A2 = A.dot(A)
-    normA2 = np.array([1.0 / np.sum(A2[i, :]) for i in range(N)])
-    normA2 = np.diag(A2)
-    A2 = normA2.dot(A2)
-    A3 = A2.dot(A)
-    normA3 = np.array([1.0 / np.sum(A3[i, :]) for i in range(N)])
-    normA3 = np.diag(A3)
-    A3 = normA3.dot(A3)
-    A4 = A3.dot(A)
-    normA4 = np.array([1.0 / np.sum(A4[i, :]) for i in range(N)])
-    normA4 = np.diag(A4)
-    A4 = normA4.dot(A4)
-    A_tot = A + A2 + A3 + A4
-    for v in range(N):
-        heat_signature.loc[v] = np.sum(A[i, :])
-
 
 def compute_heat_diff(node1, node2, mode, list_heat_df, plot=False, savefig=False):
     test1 = np.sort((list_heat_df[mode]).loc[:,node1])
